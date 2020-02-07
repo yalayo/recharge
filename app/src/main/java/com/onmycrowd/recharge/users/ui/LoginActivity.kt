d680e922ca9f21ca26f83df9e7e18d143ab5c22f
Mylab <labs@onmycrowd.com>
2020-02-06 20:41:13 -0700

    Adding login activity.

---
+++ app/src/main/java/com/onmycrowd/recharge/users/ui/LoginActivity.kt
@@ -0,0 +1,14 @@
+
+package com.onmycrowd.recharge.users.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.onmycrowd.recharge.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
